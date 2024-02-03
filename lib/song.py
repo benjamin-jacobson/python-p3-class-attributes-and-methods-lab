82c1a5916615fe765900364da4f47db6d2a2e2d5
BenjaminJacobson <ben.jacobson@flatironschool.com>
2024-02-02 20:05:18 -0500

    cmoplete lab

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,46 @@
 class Song:
-    pass
+    
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        Song.add_to_genres(self)
+        Song.add_to_artists(self)
+
+        Song.add_song_to_count(self)
+        Song.add_to_genre_count(self)
+        Song.add_to_artist_count(self)
+
+    @classmethod
+    def add_song_to_count(cls, self):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls, self):
+        cls.genres.append(self.genre)
+
+    @classmethod
+    def add_to_artists(cls, self):
+        cls.artists.append(self.artist)
+
+    @classmethod
+    def add_to_genre_count(cls,self):
+        if self.genre not in self.genre_count:
+            self.genre_count[self.genre] = 1
+        else:
+            self.genre_count[self.genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls,self):
+        if self.artist not in self.artist_count:
+            self.artist_count[self.artist] = 1
+        else:
+            self.artist_count[self.artist] += 1
+
